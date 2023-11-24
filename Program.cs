2fbce8445b2d2327a45e3a01e3850c6d05f2ea31
rfranett <rfranett@gmail.com>
2023-11-24 15:00:14 -0300

    variaveis.cs/02

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using project.net2.Models;
+
+
+string apresentaçao = "Olá,Seja bem Vindo, e veja o que eu fiz hoje .";
+
+int quantidade =1;
+
+double altura = 1.85;
+
+decimal preço = 1.80m;
+
+bool condiçao =true;
+
+Console.WriteLine(apresentaçao);
+Console.WriteLine("Valor da variável quantidade:" + quantidade);
+Console.WriteLine("Valor da variável altura:" + altura.ToString("0.000"));
+Console.WriteLine("Valor da variável preço:" + preço);
+Console.WriteLine("Valor da variável codiçao:" + condiçao);
+
+
+
+/*
+Pessoa pessoa1 = new Pessoa();
+
+pessoa1.Nome = "Franett";
+pessoa1.Idade = 50;
+pessoa1.Email = "robsomfranett@gmail.com";
+pessoa1.Apresentar();
+*/
